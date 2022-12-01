3368a29331faa11eda722c47f4e3239fbc839c7b
Samuel Sastry <anonymouscamul@gmail.com>
2022-11-30 21:11:18 -0800

    2022day1 498/1552

--- 2022/day1.js
+++ 2022/day1.js
@@ -1,1 +1,18 @@
-const input = require('fs').readFileSync(String(__dirname).replace(/\\/g,'/')+"/inputs/day_.txt", "utf8").replace(/\r/g,'')
+const input = require('fs').readFileSync(String(__dirname)
+    .replace(/\\/g,'/')+"/inputs/day1.txt", "utf8").replace(/\r/g,'')
+    .split("\n\n").map(e=>e.split("\n").map(Number).reduce((ps, a) => ps + a, 0))
+let l=[0,0,0]
+for (let i of input) {
+    if (i>l[0]) {
+        l[2]=l[1]
+        l[1]=l[0]
+        l[0]=i
+    } else if (i>l[1]) {
+        l[2]=l[1]
+        l[1]=i
+    } else if (i>l[2]) {
+        l[2]=i
+    }
+}
+console.log(Math.max(...input))
+console.log(l.reduce((ps, a) => ps + a, 0))
