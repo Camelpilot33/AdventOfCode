f2c7646d006b5c465ba61a45fde126f794bdca6c
Samuel Sastry <anonymouscamul@gmail.com>
2022-12-02 21:28:08 -0800

    1534/5232

--- 2022/day3.js
+++ 2022/day3.js
@@ -1,3 +1,28 @@
-const input = require('fs').readFileSync(String(__dirname)
-    .replace(/\\/g, '/') + "/inputs/day3.txt", "utf8").replace(/\r/g, '');
-console.log(input);
+let a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toLowerCase() + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let input = require('fs').readFileSync(String(__dirname)
+    .replace(/\\/g, '/') + "/inputs/day3.txt", "utf8").replace(/\r/g, '')
+    .split(/\n/g).map(e => e + "_");
+console.log(input.map(e => a.indexOf(e.substring(0, e.length / 2).match(
+    new RegExp("[" + e.substring(e.length / 2, e.length) + "]"))[0]
+) + 1).reduce((partialSum, a) => partialSum + a, 0));
+let b = [];
+let j = 0;
+let str = "";
+for (var i of input) {
+    str += i;
+    j++;
+    if (j == 3) {
+        j = 0;
+        b.push(str);
+        str = "";
+    }
+
+}
+console.log(b.map(e => e.split("_")).map(e => e.slice(0, e.length - 1))
+    .map(e => {
+        return a.indexOf(e[1].match(
+            new RegExp("[" + e[0] + "]", 'g')
+        ).join('').match(
+            new RegExp("[" + e[2] + "]", 'g')
+        )[0]) + 1;
+    }).reduce((partialSum, a) => partialSum + a, 0));
