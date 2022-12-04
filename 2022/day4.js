d5bbffab1eb23d9157d8e5d13ea05611afcf4f12
Samuel Sastry <anonymouscamul@gmail.com>
2022-12-03 21:12:20 -0800

    444/1261

--- 2022/day4.js
+++ 2022/day4.js
@@ -8,19 +8,19 @@ const input = require('fs').readFileSync(String(__dirname)
 .replace(/\\/g, '/') + "/inputs/day4.txt", "utf8").replace(/\r/g, '')
 .split("\n").map(e=>e.split(",").map(w=>w.split("-").map(Number)))
 let sum=0
+let sum1=0
 for (var i of input) {
+    if (i[0][0] >= i[1][0] && i[0][1] <= i[1][1] || (i[0][0] <= i[1][0] && i[0][1] >= i[1][1])) {
+        sum++
+    }
     if (
-        (i[0][0] >= i[1][0] && i[0][0]<=i[1][1])||
-        (i[0][1] >= i[1][0] && i[0][1] <= i[1][1])||
+        (i[0][0] >= i[1][0] && i[0][0] <= i[1][1]) ||
+        (i[0][1] >= i[1][0] && i[0][1] <= i[1][1]) ||
         (i[1][1] >= i[0][0] && i[1][1] <= i[0][1]) ||
         (i[1][0] >= i[0][0] && i[1][0] <= i[0][1])
-      )  {
-        sum++
+    ) {
+        sum1++;
     }
 }
 console.log(sum)
-
-
-
-
-//console.log(input.sortf())
+console.log(sum1)
