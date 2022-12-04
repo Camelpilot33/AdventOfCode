4991e08f128ef097ed4484b24e32ce844d49d95c
Samuel Sastry <anonymouscamul@gmail.com>
2022-12-03 22:26:14 -0800

    day5 prep

--- boiler.js
+++ boiler.js
@@ -1,12 +1,13 @@
-const sumArray = e => e.reduce((p, a) => p + a, 0);
 const abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-Object.defineProperty(Array.prototype, 'sortf', {
-    value: function (compare) { return [].concat(this).sort(compare); }
+Array.prototype.sum = () => { return this; };
+Object.defineProperties(Array.prototype, {
+    sum: { value: function () { return this.map(Number).reduce((p, a) => p + a, 0); } },
+    sortf: { value: function () { return this.sort((a, b) => a - b); } }
 });
 
 const input = require('fs').readFileSync(String(__dirname)
-    .replace(/\\/g, '/') + "/inputs/day_.txt", "utf8").replace(/\r/g, '')
+    .replace(/\\/g, '/') + "/inputs/day5.txt", "utf8").replace(/\r/g, '')
     .split("\n");
 
 
-console.log(input.sortf());
+console.log(input);
