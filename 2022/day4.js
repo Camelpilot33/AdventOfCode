4a8a0b43f4a102d404fac51759ac54694019c263
Samuel Sastry <anonymouscamul@gmail.com>
2022-12-03 21:09:31 -0800

    444/1261

--- 2022/day4.js
+++ 2022/day4.js
@@ -1,10 +1,26 @@
 const sumArray=e=>e.reduce((p, a) => p + a, 0);
 const abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
+Object.defineProperty(Array.prototype, 'sortf', {
+    value: function (compare) { return [].concat(this).sort(compare); }
+});
 
 const input = require('fs').readFileSync(String(__dirname)
-.replace(/\\/g, '/') + "/inputs/day4.txt", "utf8").replace(/\r/g, '');
+.replace(/\\/g, '/') + "/inputs/day4.txt", "utf8").replace(/\r/g, '')
+.split("\n").map(e=>e.split(",").map(w=>w.split("-").map(Number)))
+let sum=0
+for (var i of input) {
+    if (
+        (i[0][0] >= i[1][0] && i[0][0]<=i[1][1])||
+        (i[0][1] >= i[1][0] && i[0][1] <= i[1][1])||
+        (i[1][1] >= i[0][0] && i[1][1] <= i[0][1]) ||
+        (i[1][0] >= i[0][0] && i[1][0] <= i[0][1])
+      )  {
+        sum++
+    }
+}
+console.log(sum)
 
 
 
-console.log(input)
+
+//console.log(input.sortf())
--- boiler.js
+++ boiler.js
@@ -1,10 +1,12 @@
 const sumArray = e => e.reduce((p, a) => p + a, 0);
 const abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
+Object.defineProperty(Array.prototype, 'sortf', {
+    value: function (compare) { return [].concat(this).sort(compare); }
+});
 
 const input = require('fs').readFileSync(String(__dirname)
-.replace(/\\/g, '/') + "/inputs/day_.txt", "utf8").replace(/\r/g, '');
+    .replace(/\\/g, '/') + "/inputs/day_.txt", "utf8").replace(/\r/g, '')
+    .split("\n");
 
 
-
-console.log(input);
+console.log(input.sortf());
