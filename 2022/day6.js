997aeb4cc480a047e178e11dbdfc61405465cd4d
Samuel Sastry <anonymouscamul@gmail.com>
2022-12-05 21:07:50 -0800

    2476/2720

--- 2022/day6.js
+++ 2022/day6.js
@@ -5,9 +5,25 @@ Object.defineProperties(Array.prototype, {
     sortf: { value: function () { return this.sort((a, b) => a - b); } }
 });
 
+function isUnique (str) {
+    return new Set(str).size == str.length;
+}
+
 const input = require('fs').readFileSync(String(__dirname)
-    .replace(/\\/g, '/') + "/inputs/day6.txt", "utf8").replace(/\r/g, '')
-    .split("\n");
+.replace(/\\/g, '/') + "/inputs/day6.txt", "utf8").replace(/\r/g, '')
+let s = new Array(14).fill(-1).map((e,_)=>input[_])
+x=-1
+for (var i=4;i<input.length;i++) {
+    if (isUnique(s.join(''))) {
+        x= i
+        break
+    }
+    for (var j=0;j<13;j++) {
+        s[j]=s[j+1]
+    }
+    s[13] = input[i];
+}
 
 
-console.log(input);
+
+console.log(x);
