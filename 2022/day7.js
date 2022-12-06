bb00c936f59ffa461a61200621f911bd03fc0435
Samuel Sastry <anonymouscamul@gmail.com>
2022-12-05 21:09:05 -0800

    day7prep

---
+++ 2022/day7.js
@@ -0,0 +1,13 @@
+const abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+Array.prototype.sum = () => { return this; };
+Object.defineProperties(Array.prototype, {
+    sum: { value: function () { return this.map(Number).reduce((p, a) => p + a, 0); } },
+    sortf: { value: function () { return this.sort((a, b) => a - b); } }
+});
+
+const input = require('fs').readFileSync(String(__dirname)
+    .replace(/\\/g, '/') + "/inputs/day7.txt", "utf8").replace(/\r/g, '')
+    .split("\n");
+
+
+console.log(input);
