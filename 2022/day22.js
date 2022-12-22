b355f78394f88107dfd64d2663782a47bf33c445
Samuel Sastry <anonymouscamul@gmail.com>
2022-12-21 20:53:35 -0800

    day 22 prep

---
+++ 2022/day22.js
@@ -0,0 +1,34 @@
+const abc = "0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+Object.defineProperties(Array.prototype, {
+    sum: { value: function () { return this.map(Number).reduce((p, a) => p + a, 0); } },
+    sortf: { value: function () { return this.sort((a, b) => a - b); } },
+    prod: { value: function () { return this.map(Number).reduce((p, a) => p * a, 1); } },
+    rotate: {value: function (n) { n = n % this.length;while (this.length && n < 0) n += this.length;this.push.apply(this, this.splice(0, n));return this}}
+});
+Number.prototype.mod = function (n) {
+    "use strict";
+    return ((this % n) + n) % n;
+};
+const firstn = (arr, n) => n >= 0 ? arr.sort((a, b) => b - a).slice(0, n) : [];
+const mergeint = intervals => {
+    if (intervals.length < 2) return intervals;
+    intervals.sortf();
+    const result = [];
+    let previous = intervals[0];
+    for (let i = 1; i < intervals.length; i += 1) {
+        if (previous[1] >= intervals[i][0]) previous = [previous[0], Math.max(previous[1], intervals[i][1])];
+        else {
+            result.push(previous);
+            previous = intervals[i];
+        }
+    }
+    result.push(previous);
+    return result;
+};
+const input = require('fs').readFileSync(`${String(__dirname).replace(/\\/g, '/')}/inputs/${__filename.split('\\')[__filename.split('\\').length - 1].split('.')[0]}.txt`, "utf8").trim().replace(/\r/g, '');
+let dat = input.split('\n')
+
+
+
+
+console.log(dat)
