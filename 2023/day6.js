c61f4530e7c19ab540265e9afc73ecfaed34bc0c
Samuel Sastry <anonymouscamul@gmail.com>
2023-12-05 21:39:27 -0800

    day2023 1392/564

---
+++ 2023/day6.js
@@ -0,0 +1,50 @@
+const { get } = require('http');
+
+Object.defineProperties(Array.prototype, {
+    sum: { value: function () { return this.map(Number).reduce((p, a) => p + a, 0); } },
+    sortf: { value: function () { return this.sort((a, b) => a - b); } },
+    prod: { value: function () { return this.map(Number).reduce((p, a) => p * a, 1); } },
+    rotate: { value: function (n) { n = n % this.length; while (this.length && n < 0) n += this.length; this.push.apply(this, this.splice(0, n)); return this; } },
+    firstn: { value: function (n) { return n >= 0 ? this.sort((a, b) => b - a).slice(0, n) : []; } }
+});
+Number.prototype.mod = function (n) { return ((this % n) + n) % n; };
+const mergeint = intervals => {
+    if (intervals.length < 2) return intervals;
+    intervals.sortf();
+    const result = [];
+    let previous = intervals[0];
+    for (let i = 1; i < intervals.length; i += 1) {
+        if (previous[1] >= intervals[i][0]) previous = [previous[0], Math.max(previous[1], intervals[i][1])];
+        else {
+            result.push(previous);
+            previous = intervals[i];
+        }
+    }
+    result.push(previous);
+    return result;
+};
+const minAreaRect = (points) => (Math.max(...points.map(e => e[0])) - Math.min(...points.map(e => e[0]))) * (Math.max(...points.map(e => e[1])) - Math.min(...points.map(e => e[1])));
+const abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const log = (...a)=>{for(b of a)console.log(JSON.stringify(b, null, 2))}
+const input = require('fs').readFileSync(`${String(__dirname).replace(/\\/g, '/')}/inputs/${__filename.split('\\')[__filename.split('\\').length - 1].split('.')[0]}.txt`, 'utf8').replace(/\r/g, '');
+
+let a=input.split('\n').map(e=>e.split(/: +/g)[1].split(/ +/g).map(Number))
+let b=input.split('\n').map(e=>[e.split(/: +/g)[1].replace(/ */g,'')].map(Number))
+function gettime(t,d) {
+    let s=0
+    for (let i=0;i<=t;i++) {
+        if (d<i*(t-i)) {
+            s++
+        }
+    }
+    return s
+}
+
+let m=1
+for (i in a[0]) {
+    m*=gettime(a[0][i],a[1][i])
+}
+
+log(
+    m,gettime(b[0][0],b[1][0])
+)
