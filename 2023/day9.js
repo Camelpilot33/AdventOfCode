ee836beffc08a2a19e180a059a04fec532537ad3
Samuel Sastry <anonymouscamul@gmail.com>
2023-12-08 21:31:23 -0800

    2023day9 1272/978

---
+++ 2023/day9.js
@@ -0,0 +1,43 @@
+Object.defineProperties(Array.prototype, {
+    sum: { value: function () { return this.map(Number).reduce((p, a) => p + a, 0); } },
+    sortf: { value: function () { return this.sort((a, b) => a - b); } },
+    prod: { value: function () { return this.map(Number).reduce((p, a) => p * a, 1); } },
+    rotate: { value: function (n) { n = n % this.length; while (this.length && n < 0) n += this.length; this.push.apply(this, this.splice(0, n)); return this; } },
+    firstn: { value: function (n) { return n >= 0 ? this.sort((a, b) => b - a).slice(0, n) : []; } }
+});
+Number.prototype.mod = function (n) { return ((this % n) + n) % n; };
+const mergeint = intervals => {
+    if (intervals.length < 2) return intervals;
+    intervals.sortf();
+    const result = [];
+    let previous = intervals[0];
+    for (let i = 1; i < intervals.length; i += 1) {
+        if (previous[1] >= intervals[i][0]) previous = [previous[0], Math.max(previous[1], intervals[i][1])];
+        else {
+            result.push(previous);
+            previous = intervals[i];
+        }
+    }
+    result.push(previous);
+    return result;
+};
+const minAreaRect = (points) => (Math.max(...points.map(e => e[0])) - Math.min(...points.map(e => e[0]))) * (Math.max(...points.map(e => e[1])) - Math.min(...points.map(e => e[1])));
+const abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const log = (...a)=>{for(b of a)console.log(JSON.stringify(b, null, 2))}
+const input = require('fs').readFileSync(`${String(__dirname).replace(/\\/g, '/')}/inputs/${__filename.split('\\')[__filename.split('\\').length - 1].split('.')[0]}.txt`, 'utf8').replace(/\r/g, '');
+
+
+let a=input.split('\n').map(e=>e.split(' ').map(Number))
+f=(x)=>{
+    let s=0
+    for (i of a) {
+        let c=i[i.length-1]
+        while (!(Array.from(new Set(i)).length==1&&Array.from(new Set(i))[0]==0)) {
+            i=i.map((e,_)=>_==i.length-1?0:i[_+1]-e).slice(0, -1)
+            c+=i[i.length-1]
+        }
+        s+=c
+    }
+    return s
+}
+log(f(a),f(Array.from(a).map(e=>e.reverse())))
