e0c1d8b8765e0496c7af237bcc398391f9fc1445
Samuel Sastry <anonymouscamul@gmail.com>
2024-12-02 21:47:09 -0800

    73/104

---
+++ 2024/day3.js
@@ -0,0 +1,48 @@
+Object.defineProperties(Array.prototype, {
+    sum: { value: function () { return this.map(Number).reduce((p, a) => p + a, 0); } },
+    sortf: { value: function () { return this.sort((a, b) => a - b); } },
+    prod: { value: function () { return this.map(Number).reduce((p, a) => p * a, 1); } },
+    rotate: { value: function (n) { n = n % this.length; while (this.length && n < 0) n += this.length; this.push.apply(this, this.splice(0, n)); return this; } },
+    firstn: { value: function (n) { return n >= 0 ? this.sort((a, b) => b - a).slice(0, n) : []; } },
+    occur: { value: function () { return this.reduce((acc, curr) => { acc[curr] = acc[curr] + 1 || 1; return acc }, {}); } }
+});
+Number.prototype.mod = function (n) { return ((this % n) + n) % n; };
+const mergeint = intervals => {
+    if (intervals.length < 2) return intervals;
+    intervals.sortf();
+    const result = [];
+    let previous = intervals[0];
+    for (let i = 1; i < intervals.length; i += 1) {
+        if (previous[1] >= intervals[i][0]) previous = [previous[0], Math.max(previous[1], intervals[i][1])];
+        else {
+            result.push(previous);
+            previous = intervals[i];
+        }
+    }
+    result.push(previous);
+    return result;
+};
+const gcd = (alpha, beta) => alpha ? gcd(beta % alpha, alpha) : beta;
+const lcm = (alpha, beta) => alpha * beta / gcd(alpha, beta);
+const minAreaRect = (points) => (Math.max(...points.map(e => e[0])) - Math.min(...points.map(e => e[0]))) * (Math.max(...points.map(e => e[1])) - Math.min(...points.map(e => e[1])));
+const abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const log = (...alpha) => { for (beta of alpha) console.log(JSON.stringify(beta, null, 2)) }
+const input = require('fs').readFileSync(`${String(__dirname).replace(/\\/g, '/')}/inputs/${__filename.split('\\')[__filename.split('\\').length - 1].split('.')[0]}.txt`, 'utf8').replace(/\r/g, '');
+
+
+
+console.log(
+    input.match(/mul\(\d+,\d+\)/g).map(x=>x.match(/\d+/g)).map(x=>x[0]*x[1]).sum()
+)
+
+
+let match = input.match(/mul\(\d+,\d+\)|do\(\)|don't\(\)/g);
+let s=0;
+let enable = true
+for (v of match) {
+    if (v == "do()") enable = true;
+    else if (v == "don't()") enable = false;
+    else if (enable) s += Number(v.match(/\d+/g)[0]) * Number(v.match(/\d+/g)[1]);
+}
+
+console.log(s)
